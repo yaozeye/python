9538a497ee39eb96b221620ec380d1d1c9edf5aa
yaozeye <yaozeye@outlook.com>
2020-03-05 15:40:21 +0800

    Create circle.py

---
+++ learner/exercise/circle.py
@@ -0,0 +1,12 @@
+# Code by Yaoze Ye 
+# https://yaozeye.github.io
+# 05 March, 2020 
+import turtle
+for i in range(9):
+    turtle.circle((i+1)*20)
+    turtle.penup()
+    turtle.goto(0,-(i+1)*20)
+    turtle.pendown()
+turtle.penup()
+turtle.goto(0,20)
+turtle.done()
