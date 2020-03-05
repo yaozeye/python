2b4dd9cf36d4baebbbffae367e406e91c7718b89
yaozeye <yaozeye@outlook.com>
2020-03-05 16:52:49 +0800

    Update triangle.py

--- learner/exercise/triangle.py
+++ learner/exercise/triangle.py
@@ -2,6 +2,7 @@
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
 # 05 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 import turtle
 turtle.fd(100)
 turtle.seth(120)
