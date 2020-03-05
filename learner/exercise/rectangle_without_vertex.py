cb1e0fc3bcbb5f1b0435615c3c62cfce794087fa
yaozeye <yaozeye@outlook.com>
2020-03-05 16:56:12 +0800

    Update rectangle_without_vertex.py

--- learner/exercise/rectangle_without_vertex.py
+++ learner/exercise/rectangle_without_vertex.py
@@ -1,14 +1,14 @@
-# Python Turtle Rectangle Without Vertex
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
 # 05 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 import turtle
 for i in (90,180,270,0):
     turtle.penup()
-    turtle.fd(80)
+    turtle.fd(30)
     turtle.pendown()
-    turtle.fd(160)
+    turtle.fd(60)
     turtle.penup()
-    turtle.fd(80)
+    turtle.fd(30)
     turtle.seth(i)
 turtle.done
