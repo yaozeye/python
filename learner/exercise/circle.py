bd60d772567e4f2d963a1a33214e38cbcb8c2c15
yaozeye <yaozeye@outlook.com>
2020-03-05 16:52:06 +0800

    Update circle.py

--- learner/exercise/circle.py
+++ learner/exercise/circle.py
@@ -2,12 +2,13 @@
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
 # 05 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 import turtle
 for i in range(9):
-    turtle.circle((i+1)*20)
+    turtle.circle((i+1)*15)
     turtle.penup()
-    turtle.goto(0,-(i+1)*20)
+    turtle.goto(0,-(i+1)*15)
     turtle.pendown()
 turtle.penup()
-turtle.goto(0,20)
+turtle.goto(0,15)
 turtle.done()
