460a5d9f54cdc1c90b5422a9cd43b0931531719d
yaozeye <yaozeye@outlook.com>
2020-03-05 16:55:50 +0800

    Update hexagon.py

--- learner/exercise/hexagon.py
+++ learner/exercise/hexagon.py
@@ -2,29 +2,30 @@
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
 # 05 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 import turtle
 # First Triangle
 turtle.seth(30)
-turtle.fd(300)
+turtle.fd(120)
 turtle.seth(150)
-turtle.fd(300)
+turtle.fd(120)
 turtle.seth(270)
-turtle.fd(300)
+turtle.fd(120)
 # Re-position
 turtle.penup()
 turtle.seth(30)
-turtle.fd(100)
+turtle.fd(40)
 turtle.seth(-30)
 turtle.pendown()
 # Second Triangle
-turtle.fd(100)
+turtle.fd(40)
 turtle.seth(90)
-turtle.fd(300)
+turtle.fd(120)
 turtle.seth(210)
-turtle.fd(300)
+turtle.fd(120)
 turtle.seth(330)
-turtle.fd(200)
+turtle.fd(80)
 # Re-position
 turtle.penup()
-turtle.goto(0,300)
+turtle.goto(0,120)
 turtle.done
