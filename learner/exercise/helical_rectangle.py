99effd29adf98dad4e3f642f2d88ab8179a9fc6f
yaozeye <yaozeye@outlook.com>
2020-03-05 16:55:29 +0800

    Update helical_rectangle.py

--- learner/exercise/helical_rectangle.py
+++ learner/exercise/helical_rectangle.py
@@ -2,8 +2,9 @@
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
 # 05 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 import turtle
 for i in range(52):
-    turtle.fd((i+1)*10)
+    turtle.fd((i+1)*3)
     turtle.seth(i*90)
 turtle.done
