af8083826c0bc5121a9d20cdf0eb77d12cf8ee94
yaozeye <yaozeye@outlook.com>
2020-03-05 15:41:02 +0800

    Create triangle.py

---
+++ learner/exercise/triangle.py
@@ -0,0 +1,11 @@
+# Code by Yaoze Ye 
+# https://yaozeye.github.io
+# 05 March, 2020 
+import turtle
+turtle.fd(100)
+turtle.seth(120)
+turtle.fd(100)
+turtle.seth(240)
+turtle.fd(100)
+turtle.seth(0)
+turtle.done()
