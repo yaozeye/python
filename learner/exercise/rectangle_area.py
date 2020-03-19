9825e564e99fee8cc3525d38fa8293a540d7bea5
yaozeye <yaozeye@outlook.com>
2020-03-19 21:39:35 +0800

    Create rectangle_area.py

---
+++ learner/exercise/rectangle_area.py
@@ -0,0 +1,9 @@
+# Python Rectangle Area
+# Code by Yaoze Ye 
+# https://yaozeye.github.io
+# 19 March, 2020 
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
+length = float(input())
+width = float(input())
+area = length * width
+print("{:.2f}".format(area))
