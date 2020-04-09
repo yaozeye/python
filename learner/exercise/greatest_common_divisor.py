0de7de7c914d22f1b96dadd308455e6c35b438b9
yaozeye <yaozeye@outlook.com>
2020-04-09 21:06:06 +0800

    Create greatest_common_divisor.py

---
+++ learner/exercise/greatest_common_divisor.py
@@ -0,0 +1,14 @@
+# Python Greatest Common Divisor
+# Code by Yaoze Ye
+# https://yaozeye.github.io
+# 09 April, 2020
+# under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE
+num1 = int(input('The first number: '))
+num2 = int(input('The second number: '))
+num3 = min(num1, num2)
+for i in range(1, num3 + 1):
+    if num1 % i == 0 and num2 % i == 0:
+        common_divisor = i
+print('The maximum common divisor of %d and %d is %d.' % (num1, num2, common_divisor))
+common_multiple = (num1 * num2) / common_divisor
+print('The minimum common multiple of %d and %d is %d.' % (num1, num2, common_multiple))
