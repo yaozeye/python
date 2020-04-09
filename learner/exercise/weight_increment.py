9a279e3ae07f5e5cba018aad1db5bbc26b0d470e
yaozeye <yaozeye@outlook.com>
2020-04-09 22:17:09 +0800

    Update weight_increment.py

--- learner/exercise/weight_increment.py
+++ learner/exercise/weight_increment.py
@@ -1,12 +1,16 @@
 # Python Weight Increment
 # Code by Yaoze Ye 
 # https://yaozeye.github.io
-# 08 April, 2020 
+# 08 April 2020 
 # under the MIT License https://github.com/yaozeye/python/raw/master/LICENSE 
 weight = input()  # weight at present
 weight = eval(weight)
 print("I am now {:.2f} kg at present".format(weight))
 for i in range(1, 11, 1):
     weight = weight + 0.5
-    print("{} years later weight on earth: {:.2f} kg".format(i, weight))
-    print("{} years later weight on moon: {:.2f} kg".format(i, 0.165*weight))
+    if i == 1:
+        print("{} year later weight on earth: {:.2f} kg".format(i, weight))
+        print("{} year later weight on moon: {:.2f} kg".format(i, 0.165*weight))        
+    else:
+        print("{} years later weight on earth: {:.2f} kg".format(i, weight))
+        print("{} years later weight on moon: {:.2f} kg".format(i, 0.165*weight))
