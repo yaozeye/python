c38e88b7c26b36d4f83669bc7116fbcf7a62a272
yaozeye <yaozeye@outlook.com>
2020-04-09 21:25:24 +0800

    Update greatest_common_divisor.py

--- learner/exercise/greatest_common_divisor.py
+++ learner/exercise/greatest_common_divisor.py
@@ -8,7 +8,7 @@ num2 = int(input('The second number: '))
 num3 = min(num1, num2)
 for i in range(1, num3 + 1):
     if num1 % i == 0 and num2 % i == 0:
-        common_divisor = i
-print('The maximum common divisor of %d and %d is %d.' % (num1, num2, common_divisor))
-common_multiple = (num1 * num2) / common_divisor
-print('The minimum common multiple of %d and %d is %d.' % (num1, num2, common_multiple))
+        greatest_common_divisor = i
+print('The maximum common divisor of %d and %d is %d.' % (num1, num2, greatest_common_divisor))
+least_common_multiple = (num1 * num2) / greatest_common_divisor
+print('The minimum common multiple of %d and %d is %d.' % (num1, num2, least_common_multiple))
